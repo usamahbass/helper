212eaf1f48ae8f3a6236e69f64acc2a96364cbc4
usamahbass <basalamahusamah7@gmail.com>
2022-01-24 00:53:07 +0700

    fix: serviceworker destination

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 const nextConfig = {
   target: "serverless",
   transformManifest: (manifest) => ["/"].concat(manifest),
-  generateInDevMode: false,
+  generateInDevMode: true,
   workboxOpts: {
     swDest: "static/service-worker.js",
     runtimeCaching: [
@@ -26,16 +26,6 @@ const nextConfig = {
         },
       },
     ],
-    webpack: (config, { isServer }) => {
-      // Fixes npm packages that depend on `fs` module
-      if (!isServer) {
-        config.node = {
-          fs: "empty",
-        };
-      }
-
-      return config;
-    },
   },
 };
 
